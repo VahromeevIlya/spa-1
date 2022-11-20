0ad9f5c8581297ee4f501435a59fb356a47b3b12
Ilya <ilya.vahromeev@widestudio.ru>
2022-11-20 14:35:08 +0300

    a bit form change placement callback

--- src/components/popup/PopupCallback.tsx
+++ src/components/popup/PopupCallback.tsx
@@ -1,10 +1,5 @@
-import Tippy from "@tippyjs/react";
-import clsx from "clsx";
-import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik";
-import React from "react";
-import { PhoneInput, validateFrom1, Values } from "../order/Order";
+import FormCallback from "../forms/FormCallback";
 import { SimpleAnimatedModal } from "../SimpleAnimatedModal";
-import Popup from "./Popup";
 
 type Props = {
 	opened: boolean;
@@ -15,107 +10,7 @@ const PopupCallback = ({ opened, setOpened }: Props) => {
 	return (
 		<SimpleAnimatedModal opened={opened} onClose={() => setOpened(false)}>
 			<div className="popup__body form form--dark">
-				<Formik
-					initialValues={{
-						firstName: "",
-						phone: "",
-					}}
-					validate={validateFrom1}
-					onSubmit={(
-						values: Values,
-						{ setSubmitting, resetForm }: FormikHelpers<Values>
-					) => {
-						setTimeout(() => {
-							setSubmitting(false);
-							setOpened(true);
-							resetForm();
-							setTimeout(() => {
-								setOpened(false);
-							}, 4000);
-						}, 1000);
-					}}
-				>
-					{({ errors, touched, isSubmitting }) => (
-						<Form
-							className={clsx(
-								"form form--dark",
-								isSubmitting && "submitting"
-							)}
-						>
-							<div className="form__headline headline">
-								<h2 className="headline__title">Заказать котельную</h2>
-								<p className="headline__subtitle">
-									Оставьте свои контакты и наш менеджер свяжется с Вами
-									в ближайшее время
-								</p>
-							</div>
-							<div className="form__row">
-								<div className="form__line">
-									<Field
-										className={clsx(
-											"input",
-											errors.firstName &&
-												touched.firstName &&
-												"wpcf7-not-valid"
-										)}
-										name="firstName"
-										placeholder="Ваше имя*"
-									/>
-									<ErrorMessage name="firstName">
-										{(msg) => (
-											<Tippy className="tippy-my" content={msg}>
-												<button
-													type="button"
-													className="wpcf7-not-valid-tip"
-												></button>
-											</Tippy>
-										)}
-									</ErrorMessage>
-								</div>
-								<div className="form__line">
-									{/*<Field
-											className={clsx(
-												"input",
-												errors.phone &&
-													touched.phone &&
-													"wpcf7-not-valid"
-											)}
-											type="tel"
-											name="phone"
-											placeholder="Номер телефона*"
-										/>*/}
-									<PhoneInput name="phone" />
-									<ErrorMessage name="phone">
-										{(msg) => (
-											<Tippy className="tippy-my" content={msg}>
-												<button
-													type="button"
-													className="wpcf7-not-valid-tip"
-												></button>
-											</Tippy>
-										)}
-									</ErrorMessage>
-								</div>
-							</div>
-							<div className="form__button">
-								<button type="submit" className="button button--large">
-									Отправить
-								</button>
-							</div>
-							<div className="form__agreement">
-								Отправляя форму, Вы принимаете
-								<a href="/" target="_blank">
-									условия пользовательского соглашения
-								</a>
-								и согласны с
-								<a href="/" target="_blank">
-									политикой конфиденциальности
-								</a>
-								сайта
-							</div>
-						</Form>
-					)}
-				</Formik>
+				<FormCallback opened={opened} setOpened={setOpened} classForm="form--dark" />
 			</div>
 		</SimpleAnimatedModal>
 	);
